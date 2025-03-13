65f31e1e361116bee7c97bb6b12931433e4d5ffa
honeygola05 <honeygola05@gmail.com>
2025-03-13 09:28:10 +0530

    Added export and import functionality in user-list, admin-list, and products list

--- resources/views/admin/adminList.blade.php
+++ resources/views/admin/adminList.blade.php
@@ -73,8 +73,27 @@
                      </div>
                      <div class="btn-group">
                         <button style="padding: 3px 10px 0px 13px; margin-right: 4px;" class="btn btn-primary " type="button"><i class="fa-solid fa-rotate"></i></button>
- 
-                        
+
+                        <button style="padding: 4px;" class=" dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false"><i
+                              class="fa-solid fa-share-from-square"></i></button>
+                        <ul class="dropdown-menu dropdown-block">
+                           <div id="exportOptions">
+                              <label><input type="checkbox" class="exportField" value="ID" checked> ID</label>
+                              <label><input type="checkbox" class="exportField" value="Admin User ID" checked> Admin User ID</label>
+                              <label><input type="checkbox" class="exportField" value="Admin User Type" checked> Admin User Type</label>
+                              <label><input type="checkbox" class="exportField" value="Name" checked> Name</label>
+                              <label><input type="checkbox" class="exportField" value="Email" checked> Email</label>
+                              <label><input type="checkbox" class="exportField" value="Phone" checked> Phone</label>
+                           </div>
+                           <!-- <button onclick="exportToExcel('basic-1')">Export Excel</button>
+                           <button onclick="exportToCSV('basic-1')">Export CSV</button>
+                           <button onclick="exportToPDF('basic-1')">Export PDF</button> -->
+
+                           <li><a class="dropdown-item" href="javascript:;" onclick="exportToExcel('basic-1')">Excel</a></li>
+                           <li><a class="dropdown-item" href="javascript:;" onclick="exportToCSV('basic-1')">CSV</a></li>
+                           <li><a class="dropdown-item" href="javascript:;" onclick="exportToPDF('basic-1')">PDF</a></li>
+                           <li><a class="dropdown-item" href="javascript:;" onclick="importCSV()">Import CSV</a></li>
+                        </ul>
                         <a class="btn btn-primary mx-auto " href="{{ route('admin.add') }}"><i class="fa-solid fa-plus"></i></a>
                      </div>
                   </div>
@@ -125,6 +144,7 @@
    </div>
    @include('admin.partials.footer')
 </div>
+
 <script>
    function handleDelete(event, element) {
       event.preventDefault();
@@ -176,5 +196,107 @@
             });
          }
       });
+   }
+
+   function getSelectedFields() {
+      let selectedFields = [];
+      document.querySelectorAll(".exportField:checked").forEach(checkbox => {
+         selectedFields.push(checkbox.value);
+      });
+      return selectedFields;
+   }
+
+   function filterTableBySelectedFields(table) {
+      let selectedFields = getSelectedFields();
+      let headers = table.querySelectorAll("thead tr th");
+      let columnsToKeep = [];
+
+      headers.forEach((th, index) => {
+         if (selectedFields.includes(th.innerText.trim())) {
+            columnsToKeep.push(index);
+         }
+      });
+
+      let rows = table.rows;
+      for (let row of rows) {
+         let cells = row.cells;
+         for (let i = cells.length - 1; i >= 0; i--) {
+            if (!columnsToKeep.includes(i)) {
+               row.deleteCell(i);
+            }
+         }
+      }
+   }
+
+   function exportToExcel(tableID, filename = 'Admin_List') {
+      let table = document.getElementById(tableID);
+      filterTableBySelectedFields(table);
+      let ws = XLSX.utils.table_to_sheet(table);
+      let wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+      XLSX.writeFile(wb, `${filename}.xlsx`);
+   }
+
+   function exportToCSV(tableID, filename = 'Admin_List') {
+      let table = document.getElementById(tableID);
+      filterTableBySelectedFields(table);
+      let ws = XLSX.utils.table_to_sheet(table);
+      let csv = XLSX.utils.sheet_to_csv(ws);
+      let blob = new Blob([csv], {
+         type: 'text/csv'
+      });
+      let link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = `${filename}.csv`;
+      link.click();
+   }
+
+   function exportToPDF(tableID, filename = 'Admin_List') {
+      const {
+         jsPDF
+      } = window.jspdf;
+      let doc = new jsPDF();
+      let table = document.getElementById(tableID);
+      filterTableBySelectedFields(table);
+
+      let rows = [];
+      let headers = [];
+
+      table.querySelectorAll("thead tr th").forEach(th => headers.push(th.innerText));
+      table.querySelectorAll("tbody tr").forEach(tr => {
+         let rowData = [];
+         tr.querySelectorAll("td").forEach(td => rowData.push(td.innerText));
+         rows.push(rowData);
+      });
+
+      doc.autoTable({
+         head: [headers],
+         body: rows,
+         theme: 'grid'
+      });
+      doc.save(`${filename}.pdf`);
+   }
+
+   function importCSV() {
+      let input = document.createElement('input');
+      input.type = 'file';
+      input.accept = '.csv';
+      input.onchange = function(event) {
+         let file = event.target.files[0];
+         let reader = new FileReader();
+
+         reader.onload = function(e) {
+            let data = e.target.result;
+            let workbook = XLSX.read(data, {
+               type: 'binary'
+            });
+            let sheet = workbook.Sheets[workbook.SheetNames[0]];
+            let jsonData = XLSX.utils.sheet_to_json(sheet);
+
+            console.log("Parsed CSV Data:", jsonData);
+         };
+         reader.readAsBinaryString(file);
+      };
+      input.click();
    }
 </script>
